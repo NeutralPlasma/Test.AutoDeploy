8023b8a040111de665a861a1478e7e9bef103fe5
Gasper Lukman <gaspercino.lukman@gmail.com>
2024-08-09 08:25:39 +0200

    Add project files.

---
+++ Test.AutoDeploy/JustATest.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Test.AutoDeploy
+{
+    public static class JustATest
+    {
+
+        public static void TestATest()
+        {
+            Debug.WriteLine("Test");
+        }
+    }
+}
